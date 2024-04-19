bf1222091e7b39f58bb99a75cfe8037d2ce11260
pshaung <speshiou@gmail.com>
2024-04-20 07:02:50 +0800

    Add capability to publish instantly

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,78 +1,106 @@
-import { createJob, dispatchJob, updateJobStatus } from "@/lib/data";
+import {
+  createJob,
+  dispatchJob,
+  getJobById,
+  updateJobStatus,
+} from "@/lib/data";
 import { getSignedUrl, upload } from "@/lib/gcs";
 import TelegramApi from "@/lib/telegram/api";
 import { postTweet } from "@/lib/twitter";
 import { dateStamp } from "@/lib/utils";
 import { randomUUID } from "crypto";
+import { WithId } from "mongodb";
 
-export const dynamic = 'force-dynamic'
+export const dynamic = "force-dynamic";
+
+const processJob = async (job: WithId<Job>) => {
+  const imageTasks = job.images?.map((image) => {
+    return getSignedUrl(image);
+  });
+
+  const imageUrls = await Promise.all(imageTasks || []);
+  try {
+    const api = new TelegramApi(process.env.TELEGRAM_BOT_API_TOKEN!);
+    await api.sendMediaGroup(
+      parseInt(process.env.TELEGRAM_CHAT_ID!),
+      imageUrls,
+      job.text
+    );
+  } catch (e) {
+    console.log(e);
+  }
+
+  try {
+    await postTweet(job.text, imageUrls);
+  } catch (e) {
+    console.log(e);
+  }
+
+  await updateJobStatus(job._id, "succeeded");
+};
 
 export async function GET(request: Request) {
-    const job = await dispatchJob()
-    if (!job) {
-        return Response.json({ "status": "NO_JOBS" })
-    }
+  const job = await dispatchJob();
+  if (!job) {
+    return Response.json({ status: "NO_JOBS" });
+  }
 
-    const imageTasks = job.images?.map((image) => {
-        return getSignedUrl(image)
-    })
+  await processJob(job);
 
-    const imageUrls = await Promise.all(imageTasks || [])
-    try {
-        const api = new TelegramApi(process.env.TELEGRAM_BOT_API_TOKEN!)
-        await api.sendMediaGroup(parseInt(process.env.TELEGRAM_CHAT_ID!), imageUrls, job.text)
-    } catch (e) {
-        console.log(e)
-    }
-    
-    try {
-        await postTweet(job.text, imageUrls);
-    } catch (e) {
-        console.log(e)
-    }
-
-    await updateJobStatus(job._id, "succeeded")
-    return Response.json({ "status": "SUCCESS" })
+  return Response.json({ status: "SUCCESS" });
 }
 
 export async function POST(request: Request) {
-    // TODO: auth checking
-    const formData = await request.formData()
-    const content = formData.get('content')
-    const images = formData.getAll("image")
+  // TODO: auth checking
+  const formData = await request.formData();
+  const content = formData.get("content");
+  const images = formData.getAll("image");
+  const instant = formData.get("instant");
 
-    if (!content || !images) {
-        return Response.json({ "status": "error" }, {
-            // accpet
-            status: 400,
-          })
+  if (!content || !images) {
+    return Response.json(
+      { status: "error" },
+      {
+        // accpet
+        status: 400,
+      }
+    );
+  }
+
+  let imageUrls: string[] = [];
+  if (images) {
+    for (const file of images) {
+      if (file instanceof File) {
+        const buffer = Buffer.from(await file.arrayBuffer());
+        const filename = `${dateStamp()}/${randomUUID()}.png`;
+        const blob = new Blob([buffer]);
+        const url = await upload(filename, blob);
+        imageUrls.push(url);
+      }
     }
+  }
 
-    let imageUrls: string[] = []
-    if (images) {
-        for (const file of images) {
-            if (file instanceof File) {
-                const buffer = Buffer.from(await file.arrayBuffer());
-                const filename =  `${dateStamp()}/${randomUUID()}.png`;
-                const blob = new Blob([buffer])
-                const url = await upload(filename, blob);
-                imageUrls.push(url)
-            }
-        }
+  const newJob = {
+    create_time: new Date(),
+    update_time: new Date(),
+    status: "scheduled",
+    text: content as string,
+    images: imageUrls,
+  } satisfies Job;
+
+  const newJobId = await createJob(newJob);
+  if (instant) {
+    const job = await getJobById(newJobId);
+    if (job) {
+      processJob(job);
     }
+  }
 
-    const newJob = {
-        create_time: new Date(),
-        update_time: new Date(),
-        status: "scheduled",
-        text: content as string,
-        images: imageUrls
-    } satisfies Job
-
-    await createJob(newJob)
-    
-    return Response.json({ "status": "accepted" }, {
-        // accepted
-        status: 202,
-      })
+  return Response.json(
+    { status: "accepted" },
+    {
+      // accepted
+      status: 202,
+    }
+  );
 }
