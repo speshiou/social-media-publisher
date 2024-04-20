1b565ec1b1aa82e025485fa2fc088915d30cfecc
pshaung <speshiou@gmail.com>
2024-04-21 07:06:39 +0800

    Fix warnings of deprecation

--- lib/twitter.ts
+++ lib/twitter.ts
@@ -1,31 +1,29 @@
-import TwitterApi, { TwitterApiTokens } from 'twitter-api-v2';
+import TwitterApi, { EUploadMimeType, TwitterApiTokens } from "twitter-api-v2";
 
-let client: TwitterApi
+let client: TwitterApi;
 
 if (process.env.X_API_KEY != null) {
-    client = new TwitterApi({
-        appKey: process.env.X_API_KEY!,
-        appSecret: process.env.X_API_SECRET_KEY!,
-        accessToken: process.env.X_ACCESS_TOKEN!,
-        accessSecret: process.env.X_ACCESS_TOKEN_SECRET!,
-    } satisfies TwitterApiTokens);
+  client = new TwitterApi({
+    appKey: process.env.X_API_KEY!,
+    appSecret: process.env.X_API_SECRET_KEY!,
+    accessToken: process.env.X_ACCESS_TOKEN!,
+    accessSecret: process.env.X_ACCESS_TOKEN_SECRET!,
+  } satisfies TwitterApiTokens);
 }
 
-  
-
 export async function postTweet(content: string, images: string[]) {
-    const tasks = images.map(async (imageUrl) => {
-      const fileBlob = await fetch(new URL(imageUrl))
-    
-      const buffer = Buffer.from(await fileBlob.arrayBuffer())
-      return client.v1.uploadMedia(buffer, { type: 'png' });
-    })
+  const tasks = images.map(async (imageUrl) => {
+    const fileBlob = await fetch(new URL(imageUrl));
+
+    const buffer = Buffer.from(await fileBlob.arrayBuffer());
+    return client.v1.uploadMedia(buffer, { mimeType: EUploadMimeType.Png });
+  });
   // First, post all your images to Twitter
   const mediaIds = await Promise.all(tasks);
-  
+
   // mediaIds is a string[], can be given to .tweet
   await client.v2.tweet({
     text: content,
-    media: { media_ids: mediaIds }
+    media: { media_ids: mediaIds },
   });
 }
