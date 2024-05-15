7083ee6ff3e03ea0d462ce3ffabdeeee0b92b35a
pshaung <speshiou@gmail.com>
2024-05-15 17:21:01 +0800

    Add ability to only proceed the h jobs

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,27 +1,22 @@
-import {
-  createJob,
-  dispatchJob,
-  getJobById,
-  updateJobStatus,
-} from "@/lib/data";
-import { getSignedUrl, upload } from "@/lib/gcs";
-import TelegramApi from "@/lib/telegram/api";
-import { getTwitterClient, postTweet } from "@/lib/twitter";
-import { dateStamp } from "@/lib/utils";
-import { randomUUID } from "crypto";
-import { WithId } from "mongodb";
+import { createJob, dispatchJob, getJobById, updateJobStatus } from '@/lib/data'
+import { getSignedUrl, upload } from '@/lib/gcs'
+import TelegramApi from '@/lib/telegram/api'
+import { getTwitterClient, postTweet } from '@/lib/twitter'
+import { dateStamp } from '@/lib/utils'
+import { randomUUID } from 'crypto'
+import { WithId } from 'mongodb'
 
-export const dynamic = "force-dynamic";
+export const dynamic = 'force-dynamic'
 
 const processJob = async (job: WithId<Job>) => {
-  var results: any = {};
+  var results: any = {}
   const imageTasks = job.images?.map((image) => {
-    return getSignedUrl(image);
-  });
+    return getSignedUrl(image)
+  })
 
-  const imageUrls = await Promise.all(imageTasks || []);
+  const imageUrls = await Promise.all(imageTasks || [])
   try {
-    const api = new TelegramApi(process.env.TELEGRAM_BOT_API_TOKEN!);
+    const api = new TelegramApi(process.env.TELEGRAM_BOT_API_TOKEN!)
     await api.sendMediaGroup(
       parseInt(
         job.h == true
@@ -30,70 +25,74 @@ const processJob = async (job: WithId<Job>) => {
       ),
       imageUrls,
       job.text
-    );
-    results["telegram"] = true;
+    )
+    results['telegram'] = true
   } catch (e) {
-    results["telegram"] = false;
-    console.log(e);
+    results['telegram'] = false
+    console.log(e)
   }
 
   try {
-    const client = getTwitterClient(job.h == true);
-    await postTweet(client, job.text, imageUrls);
-    results["twitter"] = true;
+    const client = getTwitterClient(job.h == true)
+    await postTweet(client, job.text, imageUrls)
+    results['twitter'] = true
   } catch (e) {
-    results["twitter"] = false;
-    console.log(e);
+    results['twitter'] = false
+    console.log(e)
   }
 
-  await updateJobStatus(job._id, "succeeded");
-  return results;
-};
+  await updateJobStatus(job._id, 'succeeded')
+  return results
+}
 
 export async function GET(request: Request) {
-  const job = await dispatchJob();
-  if (job) {
-    await processJob(job);
+  const url = new URL(request.url)
+  const searchParams = new URLSearchParams(url.search)
+  if (!searchParams.get('h')) {
+    const job = await dispatchJob()
+    if (job) {
+      await processJob(job)
+    }
   }
 
-  const jobH = await dispatchJob(true);
+  const jobH = await dispatchJob(true)
   if (jobH) {
-    await processJob(jobH);
+    await processJob(jobH)
   }
 
-  return Response.json({ status: "SUCCESS" });
+  return Response.json({ status: 'SUCCESS' })
 }
 
 export async function POST(request: Request) {
   // TODO: auth checking
-  const formData = await request.formData();
-  const content = formData.get("content");
-  const images = formData.getAll("image");
-  const instant = formData.get("instant");
-  const h = formData.get("h");
+  const formData = await request.formData()
+  const content = formData.get('content')
+  const images = formData.getAll('image')
+  const instant = formData.get('instant')
+  const h = formData.get('h')
 
   if (!content || !images) {
     return Response.json(
-      { status: "error" },
+      { status: 'error' },
       {
         // accpet
         status: 400,
       }
-    );
+    )
   }
 
-  let imageUrls: string[] = [];
+  let imageUrls: string[] = []
   if (images) {
     for (const file of images) {
       if (file instanceof File) {
-        const buffer = Buffer.from(await file.arrayBuffer());
-        let filename = `${dateStamp()}/${randomUUID()}.png`;
+        const buffer = Buffer.from(await file.arrayBuffer())
+        let filename = `${dateStamp()}/${randomUUID()}.png`
         if (h) {
-          filename = `h/${filename}`;
+          filename = `h/${filename}`
         }
-        const blob = new Blob([buffer]);
-        const url = await upload(filename, blob);
-        imageUrls.push(url);
+        const blob = new Blob([buffer])
+        const url = await upload(filename, blob)
+        imageUrls.push(url)
       }
     }
   }
@@ -101,31 +100,31 @@ export async function POST(request: Request) {
   const newJob = {
     create_time: new Date(),
     update_time: new Date(),
-    status: "scheduled",
+    status: 'scheduled',
     text: content as string,
     images: imageUrls,
     h: h != null,
-  } satisfies Job;
+  } satisfies Job
 
-  const newJobId = await createJob(newJob);
+  const newJobId = await createJob(newJob)
   if (instant) {
-    const job = await getJobById(newJobId);
+    const job = await getJobById(newJobId)
     if (job) {
-      const results = processJob(job);
+      const results = processJob(job)
       return Response.json(
-        { status: "succeeded", results: results },
+        { status: 'succeeded', results: results },
         {
           status: 200,
         }
-      );
+      )
     }
   }
 
   return Response.json(
-    { status: "accepted" },
+    { status: 'accepted' },
     {
       // accepted
       status: 202,
     }
-  );
+  )
 }
